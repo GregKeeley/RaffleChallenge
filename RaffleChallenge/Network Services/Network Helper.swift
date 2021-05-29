02374d5bd09f8f16303dcf4c41ecaebd20c942e5
GregKeeley <gregoryKeeley@pursuit.org>
2021-05-29 09:48:10 -0400

    - Added NetworkHelper layer to perform data tasks

--- RaffleChallenge/Network Services/Network Helper.swift
+++ RaffleChallenge/Network Services/Network Helper.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+enum AppError: Error {
+    case badURL(String)
+    case noResponse
+    case networkClientError(Error)
+    case noData
+    case decodingError(Error)
+    case encodingError(Error)
+    case badStatusCode(Int)
+}
+
+class NetworkHelper {
+    static let shared = NetworkHelper()
+    private var urlSession: URLSession
+    private init() {
+        urlSession = URLSession(configuration: .default)
+    }
+    public func performDataTask(with request: URLRequest, completion: @escaping (Result<Data, AppError>) -> ()) {
+        let dataTask = urlSession.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(.networkClientError(error)))
+                return
+            }
+            guard let urlResponse = response as? HTTPURLResponse else {
+                completion(.failure(.noResponse))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            switch urlResponse.statusCode {
+            case 200...299: break
+            default:
+                completion(.failure(.badStatusCode(urlResponse.statusCode)))
+                return
+            }
+            completion(.success(data))
+        }
+        dataTask.resume()
+    }
+}
